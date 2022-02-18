3f2fd7a4403160e74298238dd580fef30748065f
SrRojo <wgaliber@gmail.com>
2022-02-18 10:19:38 -0500

    Add submit functionality

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 //Variable Declaration
 let selectedPiece = "";
-let guess = 1;
-
-//Grid Declaration
-let grid = [[null, null, null, null],
-            [null, null, null, null],
-            [null, null, null, null],
-            [null, null, null, null]];
+let currentGuess = 0;
+let guesses = 0;
 
 //Piece Object Declarations
 let Pawn = {
+    name: "Pawn",
     targets: [[-1, -1], [1, -1]]
 }
 let Knight = {
+    name: "Knight",
     targets: [[-1, -2], [1, -2], [-1, 2], [1, 2],
               [-2, -1], [2, -1], [-2, 1], [2, 1]]
 }
 let Bishop = {
+    name: "Bishop",
     targets: [[-1, -1], [-2, -2], [-3, -3],
               [-1, 1], [-2, 2], [-3, 3],
               [1, -1], [2, -2], [3, -3],
               [1, 1], [2, 2], [3, 3]]
 }
 let Rook = {
+    name: "Rook",
     targets: [[-1, 0], [-2, 0], [-3, 0],
               [0, 1], [0, 2], [0, 3],
               [0, -1], [0, -2], [0, -3],
               [1, 0], [2, 0], [3, 0]]
 }
 let Queen = {
+    name: "Queen",
     targets: [[-1, 0], [-2, 0], [-3, 0],
               [0, 1], [0, 2], [0, 3],
               [0, -1], [0, -2], [0, -3],
@@ -39,10 +39,32 @@ let Queen = {
               [1, 1], [2, 2], [3, 3]]
 }
 let King = {
+    name: "King",
     targets: [[-1, -1], [-1, 0], [-1, 1],
               [0, -1], [0, 1],
               [1, -1], [1, 0], [1, 1]]
 }
+
+//Grid Declaration
+let grid = [[[null, null, null, null],
+             [null, null, null, null],
+             [null, null, null, null],
+             [null, null, null, null]]];
+
+//Test Answer
+let answer = [[null, null, null, Queen.color = "W"],
+              [null, null, null, null],
+              [Pawn.color = "B", null, null, null],
+              [King.color = "B", null, null, Rook.color = "W"]];
+/*
+let answer = [[Knight.color = "W", Bishop.color = "B", null, null],
+              [null, null, King.color = "B", null],
+              [Bishop.color = "W", null, null, null],
+              [null, Pawn.color = "W", null, Rook.color = "W"]];
+*/
+
+//Guess Progress
+let progress = [];
 
 //Select a Piece
 function selectPiece(piece) {
@@ -68,21 +90,88 @@ function assignPos(xPos, yPos) {
     if (selectedPiece != "") {
         let img = document.getElementById(xPos + "" + yPos).firstChild;
         img.src = "src/" + selectedPiece + ".png";
-        grid[yPos][xPos] = getPieceObj(selectedPiece);
+        grid[currentGuess][yPos][xPos] = getPieceObj(selectedPiece);
     } else {
         let img = document.getElementById(xPos + "" + yPos).firstChild;
         img.src = "";
-        grid[yPos][xPos] = null;
+        grid[currentGuess][yPos][xPos] = null;
     }
 }
 
 //Submit a Guess
-function submit() {
-    guess++;
-    grid = [[null, null, null, null],
-            [null, null, null, null],
-            [null, null, null, null],
-            [null, null, null, null]];
+function submitGuess() {
+
+    if (currentGuess == guesses) {
+
+        //Progress Grid Declaration
+        progress.push([["", "", "", ""],
+                       ["", "", "", ""],
+                       ["", "", "", ""],
+                       ["", "", "", ""]]);
+        guesses++;
+    }
+
+    //Validate Grid
+    for (let y = 0; y < 4; y++) {
+        for (let x = 0; x < 4; x++) {
+
+            //Check for piece in current location
+            if (grid[currentGuess][y][x] != null) {
+
+                //Check for Green Squares
+                if (answer[y][x] != null && grid[currentGuess][y][x].name == answer[y][x].name && grid[currentGuess][y][x].color == answer[y][x].color) {
+                    progress[currentGuess][y][x] = "G";
+                } else {
+                    exit:
+
+                    //Check for Blue Squares
+                    for (let i = 0; i < 4; i++) {
+                        for (let j = 0; j < 4; j++) {
+                            if (answer[j][i] != null && grid[currentGuess][y][x].name == answer[j][i].name && grid[currentGuess][y][x].color == answer[j][i].color) {
+                                progress[currentGuess][y][x] = "B";
+                                break exit;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    colorGrid();
+}
+
+function colorGrid() {
+    for (let y = 0; y < 4; y++) {
+        for (let x = 0; x < 4; x++) {
+            if (progress[currentGuess][y][x] == "G") {
+                document.getElementById(x + "" + y).classList.add("green");
+            } else if (progress[currentGuess][y][x] == "B") {
+                document.getElementById(x + "" + y).classList.add("blue");
+            }
+        }
+    }
+}
+
+//Move to Next Guess
+function nextGuess() {
+
+    //Check if moved to latest guess
+    if (currentGuess < guesses - 1) {
+        colorGrid();
+    }
+
+    //Move Guess
+    if (currentGuess < guesses) {
+        currentGuess++;
+    }
+}
+
+//Move to Previous Guess
+function previousGuess() {
+    if (currentGuess > 0) {
+        currentGuess--;
+    }
+    colorGrid();
 }
 
 //Get the positions of all targetted squares
@@ -125,12 +214,12 @@ function isMate() {
                 if (i != 0 && j != 0) {
 
                     //Loop through all squares
-                    for (let y = 0; y < grid.length; y++) {
-                        for (let x = 0; x < grid[y].length; x++) {
+                    for (let y = 0; y < grid[currentGuess].length; y++) {
+                        for (let x = 0; x < grid[currentGuess][y].length; x++) {
 
                             //Check for white pieces
-                            if (grid[y][x] != null && grid[y][x].color == "W") {
-                                let pos = getTargetPos(grid[y][x], x, y);
+                            if (grid[currentGuess][y][x] != null && grid[currentGuess][y][x].color == "W") {
+                                let pos = getTargetPos(grid[currentGuess][y][x], x, y);
                                 for (let k = 0; k < pos.length; k++) {
                                     
                                 }
